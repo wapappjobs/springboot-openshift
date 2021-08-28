c675075abdbb798700cf786725a4a03eb668b07f
wapappjobs <wapapp.jobs@gmail.com>
2021-08-28 18:36:31 +0530

    added basic controller

---
+++ src/main/java/com/traning/cloud/deployment/springbootopenshift/SpringbootOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.traning.cloud.deployment.springbootopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenshiftApplication.class, args);
+	}
+
+}
