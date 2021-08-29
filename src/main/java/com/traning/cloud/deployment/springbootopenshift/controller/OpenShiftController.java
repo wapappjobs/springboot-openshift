b919d6202bc0cd42603e648557b4f65d15a8664e
wapappjobs <wapapp.jobs@gmail.com>
2021-08-29 21:49:57 +0530

    added basic controller

--- src/main/java/com/traning/cloud/deployment/springbootopenshift/controller/OpenShiftController.java
+++ src/main/java/com/traning/cloud/deployment/springbootopenshift/controller/OpenShiftController.java
@@ -9,10 +9,13 @@ public class OpenShiftController {
 
     @GetMapping("/hello")
     public String hell(){
+
         return "Hello";
     }
     @GetMapping()
     public String test(){
+       String envDat= System.getenv().getOrDefault("test-config","LAST");
+        System.out.println("envDat==================:"+envDat);
         return "Test";
     }
 
